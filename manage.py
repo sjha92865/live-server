ee6212ba4055aec6122dce216d90755a08dd6a7a
Shubham Jha <shubham1.jha@paytm.com>
2022-08-07 17:16:32 +0530

    third

--- manage.py
+++ manage.py
@@ -3,9 +3,10 @@
 import os
 import sys
 
-
+import pathlib
 def main():
     """Run administrative tasks."""
+    
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'livepreview.settings')
     try:
         from django.core.management import execute_from_command_line
